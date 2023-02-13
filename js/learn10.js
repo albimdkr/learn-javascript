a6292557fb36536ab4b63a1fccee30c5e3ba6b66
albimdkr <albimudakar04@gmail.com>
2023-02-14 01:28:01 +0700

    learn 10, condition

---
+++ js/learn10.js
@@ -0,0 +1,30 @@
+//CONDITIONAL
+//IF Statment 1
+//Qustion : "If Rainy, you must doing ?"
+var isRaining = true;
+if (isRaining){
+    document.getElementById("IFStatment-1").innerHTML = "IF Statment 1 (TRUE) : You must bring the raincoat";
+    document.getElementById("IFStatment-1").style.backgroundColor = "green";
+    document.getElementById("IFStatment-1").style.color = "white";
+}
+
+
+
+
+
+
+
+
+
+
+// var age = prompt('enter the age');
+
+// if (age < 18){
+//     //alert("Not Allowed" + aAge);
+//     document.getElementById("IFStatment-1").innerHTML = "Not Allowed " + age;
+//     document.getElementById("IFStatment-1").style.backgroundColor = "red";
+// } else {
+//     //alert("Allowed" + age);
+//     document.getElementById("IFStatment-1").innerHTML = "Continue " + age;
+//     document.getElementById("IFStatment-1").style.backgroundColor = "green";
+// }
