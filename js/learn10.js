52701bb06d53e10650f4f8f49f11e8b9b1fa29ae
albimdkr <albimudakar04@gmail.com>
2023-02-15 23:52:17 +0700

    add switch case statment

--- js/learn10.js
+++ js/learn10.js
@@ -57,3 +57,45 @@ if (number > 0){
     document.getElementById("IF-ELSEIF-ELSE-Statment-3").style.backgroundColor = "red";
     document.getElementById("IF-ELSEIF-ELSE-Statment-3").style.color = "white";
 }
+
+//SWITCH CASE
+function getValue() {
+  const numberValue = document.getElementById("fetchValue").value;
+
+  switch (true){
+    case numberValue > 100:
+        var resultSwitchCaseValue = "Damn, your number is very big ";
+        document.getElementById("SWITCH-CASE-Statment-2").innerHTML = resultSwitchCaseValue;
+        document.getElementById("SWITCH-CASE-Statment-2").style.backgroundColor = "black";
+        document.getElementById("SWITCH-CASE-Statment-2").style.color = "white";
+    break;
+    case numberValue > 0:
+        var resultSwitchCaseValue = "This is number Positive, your value is = " + numberValue;
+        document.getElementById("SWITCH-CASE-Statment-2").innerHTML = resultSwitchCaseValue;
+        document.getElementById("SWITCH-CASE-Statment-2").style.backgroundColor = "green";
+        document.getElementById("SWITCH-CASE-Statment-2").style.color = "white";
+    break;
+    case numberValue == 0:
+        var resultSwitchCaseValue = "Sorry, your value is zero, Please enter again :( ";
+        document.getElementById("SWITCH-CASE-Statment-2").innerHTML = resultSwitchCaseValue;
+        document.getElementById("SWITCH-CASE-Statment-2").style.backgroundColor = "orange";
+        document.getElementById("SWITCH-CASE-Statment-2").style.color = "white";
+    break;
+    case numberValue < 0:
+        var resultSwitchCaseValue = "This number is Negative, your number is = " + numberValue;
+        document.getElementById("SWITCH-CASE-Statment-2").innerHTML = resultSwitchCaseValue;
+        document.getElementById("SWITCH-CASE-Statment-2").style.backgroundColor = "gray";
+        document.getElementById("SWITCH-CASE-Statment-2").style.color = "white";
+    break;
+    default :
+    var resultSwitchCaseValue = "Holy shit! what wrongs with you dude come on, this is not a number !!!";
+    document.getElementById("SWITCH-CASE-Statment-2").innerHTML = resultSwitchCaseValue;
+    document.getElementById("SWITCH-CASE-Statment-2").style.backgroundColor = "red";
+    document.getElementById("SWITCH-CASE-Statment-2").style.color = "white";
+  }
+
+
+
+  //document.getElementById("SWITCH-CASE-Statment-2").innerHTML = numberValue;
+
+}
