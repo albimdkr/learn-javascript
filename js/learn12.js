de39709ea93fdc4caa5346f5381cbb4ebf72f5f9
albimdkr <albimudakar04@gmail.com>
2023-02-20 21:47:11 +0700

    next into do while loop

--- js/learn12.js
+++ js/learn12.js
@@ -1,10 +1,13 @@
 //JAVASCRIPT LOOPING
-//structur
+
+//FOR LOOP
+//structure
+document.getElementById("structure-forloop").innerHTML = "for (initialization, condition, increamtent/decrement){ " + "<br>" + "//code here " + "<br>" + "};";
+document.getElementById("structure-forloop").style.fontWeight = "600";
+document.getElementById("structure-forloop").style.textAlign = "left";
 //for (initialization, condition, increamtent/decrement){ 
 //code here  
 //};
-
-//FOR LOOP
 //case 1 
 for (let varX = 0; varX <= 5; varX++){
     document.getElementById("forloop-1").innerHTML = 'loop-1, print varX 10 : ' + varX;
@@ -41,4 +44,35 @@ for (let i = 0; i < num1.length; i++){
 }
 document.getElementById("forloop-5").innerHTML = 'loop-5, creat array in array  : ' + numArr;
 
+//WHILE LOOP
+//structure
+document.getElementById("structure-whileloop").innerHTML = "initialization" + "<br>" + "while(conditions){" + "<br>" + "//code here"+ "<br>" + "increament/decrement" + "<br>" +"};";
+document.getElementById("structure-whileloop").style.fontWeight = "600";
+document.getElementById("structure-whileloop").style.textAlign = "left";
 
+//case 1 : while loop
+let i = 0;
+while(i <= 10){
+    document.getElementById("whileloop-1").innerHTML = "whileloop-1 : " + i;
+    i++
+};
+
+//case 2 : while loop 
+let c = 0;
+while (c <= 5){
+    var calExNum = `${c} * ${c} =  ${c * c}`;
+    document.getElementById("whileloop-2").innerHTML = "whileloop-2, use arithmetic: " + calExNum;
+    c++
+};
+
+//case 3 : while loop with array
+const numArrW = [1,2,3,4,5];
+let sumArrW = 0;
+let x = 0;
+while (x < numArrW.length){
+    sumArrW = sumArrW + numArrW[x];
+    x++
+};
+document.getElementById("whileloop-3").innerHTML = "whileloop-3, sum array : " + sumArrW;
+
+//DO WHILE LOOP
