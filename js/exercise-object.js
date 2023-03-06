8dcbb21e805a495c193dd705f55abd7be2243772
albimdkr <albimudakar04@gmail.com>
2023-03-07 01:20:39 +0700

    complete exercise level 1, objects

--- js/exercise-object.js
+++ js/exercise-object.js
@@ -10,11 +10,32 @@ console.log(dog2);
 //case 3 : 
 //Add name, legs, color, age and bark properties for the dog object. The bark property is a method which return woof woof
 var dog3 = {
-  name : 'scoobydu'
+  name : 'scoobydu',
   legs : 4,
   color : 'black, gray and white',
  	age : 5,
   bark : function (){
    	return "woof woof";
-  };
+  }
 };
+
+//case 4 : Get name, legs, color, age and bark value from the dog object
+console.log(dog3.name);
+console.log(dog3.legs);
+console.log(dog3.color);
+console.log(dog3.age);
+console.log(dog3.bark());
+
+
+//case 5 : Set new properties the dog object: breed, getDogInfo
+dog3.breed = 'Alaska Malamute'
+dog3.getDogInfo = function (){
+  var firstName = 'scoo ';
+  var lastName = 'bydu ';
+  var age = 5 ;
+  var ras = 'alaska ';
+  var food = 'like a beef '
+  return firstName+lastName+age+ras+food;
+};
+console.log(dog3.breed);
+console.log(dog3.getDogInfo());
