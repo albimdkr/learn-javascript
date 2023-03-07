187d47966de4509d5726ab5dabcdd672391b64be
albimdkr <albimudakar04@gmail.com>
2023-03-08 00:13:42 +0700

    add exercise : level 2

--- js/exercise-object.js
+++ js/exercise-object.js
@@ -39,3 +39,73 @@ dog3.getDogInfo = function (){
 };
 console.log(dog3.breed);
 console.log(dog3.getDogInfo());
+
+//EXERCISE : LEVEL 2 
+
+var users = {
+  Alex: {
+    email: 'alex@alex.com',
+    skills: ['HTML', 'CSS', 'JavaScript'],
+    age: 20,
+    isLoggedIn: false,
+    points: 30
+  },
+  Asab: {
+    email: 'asab@asab.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'Redux', 'MongoDB', 'Express', 'React', 'Node'],
+    age: 25,
+    isLoggedIn: false,
+    points: 50
+  },
+  Brook: {
+    email: 'daniel@daniel.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
+    age: 30,
+    isLoggedIn: true,
+    points: 50
+  },
+  Daniel: {
+    email: 'daniel@alex.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
+    age: 20,
+    isLoggedIn: false,
+    points: 40
+  },
+  John: {
+    email: 'john@john.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
+    age: 20,
+    isLoggedIn: true,
+    points: 50
+  },
+  Thomas: {
+    email: 'thomas@thomas.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React'],
+    age: 20,
+    isLoggedIn: false,
+    points: 40
+  },
+  Paul: {
+    email: 'paul@paul.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node'],
+    age: 20,
+    isLoggedIn: false,
+    points: 40
+  }
+};
+
+//case 1 : Find the person who has many skills in the users object.
+console.log(users.Asab);
+
+//case 2 : Count logged in users, count users having greater than equal to 50 points from the following object.
+var login = 0;
+var count = 0;
+for (user in users){
+  if (users[user].isLoggedIn){
+    login++;
+  }if(users[user].points < 50){
+     count++;
+  };
+};
+console.log(`user loggedIn is : ${login}`);
+console.log(`user haver grater 50 points is : ${count}`);
