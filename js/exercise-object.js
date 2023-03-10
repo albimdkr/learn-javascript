0fdc7b6ce8c9cf283dd52d01996b9025da11c227
albimdkr <albimudakar04@gmail.com>
2023-03-10 23:40:57 +0700

    add exercise level 3, case 2 : a

--- js/exercise-object.js
+++ js/exercise-object.js
@@ -171,3 +171,57 @@ console.log(personAccount);
 console.log(personAccount.totalIncomeExpense());
 
 
+// case 2 : **** Questions:2, 3 and 4 are based on the following two arrays:users and products
+//a : Create a function called signUp which allows user to add to the collection. If user exists, inform the user that he has already an account.
+var userss = [
+  {
+      _id: 'ab12ex',
+      username: 'Alex',
+      email: 'alex@alex.com',
+      password: '123123',
+      createdAt:'08/01/2020 9:00 AM',
+      isLoggedIn: false
+  },
+  {
+      _id: 'fg12cy',
+      username: 'Asab',
+      email: 'asab@asab.com',
+      password: '123456',
+      createdAt:'08/01/2020 9:30 AM',
+      isLoggedIn: true
+  },
+  {
+      _id: 'zwf8md',
+      username: 'Brook',
+      email: 'brook@brook.com',
+      password: '123111',
+      createdAt:'08/01/2020 9:45 AM',
+      isLoggedIn: true
+  },
+  {
+      _id: 'eefamr',
+      username: 'Martha',
+      email: 'martha@martha.com',
+      password: '123222',
+      createdAt:'08/01/2020 9:50 AM',
+      isLoggedIn: false
+  },
+  {
+      _id: 'ghderc',
+      username: 'Thomas',
+      email: 'thomas@thomas.com',
+      password: '123333',
+      createdAt:'08/01/2020 10:00 AM',
+      isLoggedIn: true
+  }
+  ];
+
+var user = 0;
+var signUp = 0;
+
+for (user in userss){
+if (userss[user].isLoggedIn){
+  signUp++;
+};
+};
+console.log(`${signUp}`);
