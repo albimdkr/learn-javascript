551a0d8fd7b88f6061ec4d20fe0f4d492029421e
albimdkr <albimudakar04@gmail.com>
2023-03-12 01:21:45 +0700

    higth order function : returning function

--- js/learn15.js
+++ js/learn15.js
@@ -13,5 +13,23 @@ function cube (callBack, n){
 //console.log("case-1 : " + cube(callBack, 20));
 document.getElementById("callback-1").innerHTML = "callback, case-1 : " + (cube(callBack, 20));
 
-// Returning Funtion
+//Returning Funtion
+//normal function
+var normalFunction = (n,m,t) => {
+    return 2 * n + 3 * m + t;
+    //proses
+    // 2*2=4, 3*3=9, 9+4=(13)+10=23; 
+};
+console.log("normal function : "+ normalFunction(2,3,10));
+
 //Higher order functions return function as a value ​
+var highOrderr = n => {
+    const doSomething = m => {
+        const doWhatEver = t => {
+            return 2 * n + 3 * m + t;
+        };
+        return doWhatEver;
+    };
+    return doSomething;
+};
+console.log("hight order function : " + highOrderr(2)(3)(10));
