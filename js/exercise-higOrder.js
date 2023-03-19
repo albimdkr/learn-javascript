a3320ee8d391ea8db114a24db6b41a0f5811b0e8
albimdkr <albimudakar04@gmail.com>
2023-03-20 01:22:16 +0700

    Exercise 1 : case 1

---
+++ js/exercise-higOrder.js
@@ -0,0 +1,18 @@
+var countries = ['Finland', 'Sweden', 'Denmark', 'Norway', 'IceLand']
+var names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+var products = [
+  { product: 'banana', price: 3 },
+  { product: 'mango', price: 6 },
+  { product: 'potato', price: ' ' },
+  { product: 'avocado', price: 8 },
+  { product: 'coffee', price: 10 },
+  { product: 'tea', price: '' },
+];
+
+//EXERCISE 1
+//case 1 : Explain the difference between forEach, map, filter, and reduce.
+//forEach : for iterate an elements 
+// map : Iterate an elemetns and modify . callback function elements with index, array params or return a new array
+//filter : to filter ouc item cindition and return a new array
+//reduce : can take callback function, the callback function takes accumlator, current, and optional value as a paramater and return a single value 
