6ca8a003ddac89c85496ec0b374424008eed34c8
albimdkr <albimudakar04@gmail.com>
2023-03-27 22:52:32 +0700

    fiks case 2 and add case 3, exercise set map

--- js/exercise-setsMaps.js
+++ js/exercise-setsMaps.js
@@ -3,9 +3,20 @@ var sets = new Set()
 console.log(sets);
 
 //case 2 : Create a set containing 0 to 10 using loop
-var numbers = [0,1,2,3,4,5,6,7,8,9,10];
-var numSet = new Set(numbers);
-for (number of numbers){
-  console.log(number);
-}
+var num = [0,1,2,3,4,5,6,7,8,9,10];
+setOfnum = new Set();
+for (var numbers of num){
+  setOfnum.add(numbers);
+};
 
+
+//case 3 : Remove an element from a set
+var countriess = new Set();
+console.log(countriess.size);
+
+countriess.add('Swedia');
+countriess.add('England');
+//console.log(countriess);
+
+countriess.delete('Swedia');
+console.log(countriess);
