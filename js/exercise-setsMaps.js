d459befec9a78ac907938471a2d4df288d01ba52
albimdkr <albimudakar04@gmail.com>
2023-03-28 00:02:09 +0700

    finish exercise level 1

--- js/exercise-setsMaps.js
+++ js/exercise-setsMaps.js
@@ -9,14 +9,40 @@ for (var numbers of num){
   setOfnum.add(numbers);
 };
 
-
 //case 3 : Remove an element from a set
 var countriess = new Set();
 console.log(countriess.size);
-
 countriess.add('Swedia');
 countriess.add('England');
-//console.log(countriess);
-
 countriess.delete('Swedia');
 console.log(countriess);
+
+//case 4 : clear a set
+var countriess = new Set();
+console.log(countriess.size);
+countriess.add('Swedia');
+countriess.add('England');
+countriess.clear();
+console.log(countriess);
+
+//case 5 : create a set of 5 string elements from array
+var languages = [
+  'English',
+  'Finnish',
+  'Indonesian',
+  'English',
+  'French',
+  'Indonesian',
+  'Spanish',
+  'English',
+  'French',
+]
+var newArr = new Set(languages);
+console.log(newArr);
+
+// case 6 : Create a map of countries and number of characters of a country
+var countriesMap = new Map();
+countriesMap.set('Indonesian','Jakarta');
+countriesMap.set('Thailand', 'Bangkok');
+console.log(countriesMap);
+console.log(countriesMap.size);
