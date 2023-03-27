1532763efc2e05bc31fe1b8538341e3e3e44d3bd
albimdkr <albimudakar04@gmail.com>
2023-03-27 22:38:18 +0700

    exercise sets and map

---
+++ js/exercise-setsMaps.js
@@ -0,0 +1,11 @@
+//case 1 : creat an empty set 
+var sets = new Set()
+console.log(sets);
+
+//case 2 : Create a set containing 0 to 10 using loop
+var numbers = [0,1,2,3,4,5,6,7,8,9,10];
+var numSet = new Set(numbers);
+for (number of numbers){
+  console.log(number);
+}
+
