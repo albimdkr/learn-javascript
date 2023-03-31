ada68524d72ac83e9967da36919650f57c26e6a5
albimdkr <albimudakar04@gmail.com>
2023-03-31 23:25:54 +0700

    add Destructuring object during iteration

--- js/learn17.js
+++ js/learn17.js
@@ -146,3 +146,25 @@ const getPersonInfo1 = ({
   return personInfo1;
 }
 console.log(getPersonInfo1(person));
+
+//Destructuring object during iteration
+const todoList = [
+  {
+    task:'Prepare JS Test',
+    time:'4/1/2020 8:30',
+    completed:true
+  },
+  {
+    task:'Give JS Test',
+    time:'4/1/2020 10:00',
+    completed:false
+  },
+  {
+    task:'Assess Test Result',
+    time:'4/1/2020 1:00',
+    completed:false
+  }
+];
+for (const {task, time, completed} of todoList){
+  console.log(task, time, completed);
+}
