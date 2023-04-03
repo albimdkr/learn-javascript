e314648d2f97f54e6ed2b549bf86ad9f9e89070b
albimdkr <albimudakar04@gmail.com>
2023-04-03 21:38:41 +0700

    add exercise level 1 : case 1

---
+++ js/exercise-destructuring-spread.js
@@ -0,0 +1,5 @@
+//Exercises: Level 1
+//case 1 : Destructure and assign the elements of constants array to e, pi, gravity, humanBodyTemp, waterBoilingTemp.
+var constants = [2.72, 3.14, 9.81, 37, 100];
+let [pi1,pi2,pi3,pi4,pi5] = constants;
+console.log(pi1,pi2,pi3,pi4,pi5,pi5);
