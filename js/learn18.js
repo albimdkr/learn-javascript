caf7b349aecb7da6f6749d42215c1c6966635b7c
albimdkr <albimudakar04@gmail.com>
2023-04-06 22:29:00 +0700

    to RegExp Object Methods

--- js/learn18.js
+++ js/learn18.js
@@ -10,3 +10,14 @@ let pattern2 = 'love';
 let flag = 'gi';
 let regEx2 = new RegExp(pattern, flag);
 console.log(regEx2);
+
+// Declaring the regex pattern using RegEx object. Writing the pattern and the flag inside the RegExp Contructor
+let regEx3 = new RegExp('love', 'gi');
+console.log(regEx3);
+
+// Creating A Pattern Without RegExp Constructor
+///let regEx4 = /love/gi;
+//The above regular expression is the same as the one which we created with RegExp constructor
+let regEx4 = new RegExp('love', 'gi');
+console.log(regEx4);
+
