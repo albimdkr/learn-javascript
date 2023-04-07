e5f007484f6c95b7db2bc3e56081a104aa698cf3
albimdkr <albimudakar04@gmail.com>
2023-04-07 19:44:47 +0700

    add styling css in console

--- js/learn19.js
+++ js/learn19.js
@@ -4,3 +4,7 @@ console.log('30 Days Of JavaScript');
 
 //subtitution
 console.log('%d %s of JavaScript', 30, 'Days');
+
+//CSS
+console.log('%c30 Days Of JavaScript', 'color:green');
+console.log('%c30 Days%c %cOf%c %cJavaScript%c', 'color:green', '', 'color:red', '', 'color:yellow');
