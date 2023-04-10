c4bae77a44f599c82c8fffec86b1c6c56dbe7e27
albimdkr <albimudakar04@gmail.com>
2023-04-11 00:03:18 +0700

    add Constructor with value

--- js/learn20.js
+++ js/learn20.js
@@ -18,4 +18,15 @@ class Myperson1 {
     };
 };
 const myPerson1 = new Myperson1();
-console.log(myPerson1);
+console.log(myPerson1); //undifined
+//after instantiate the class
+class Myperson2 {
+    constructor (firstName, lastName){
+        console.log(this); //check the output from here
+        this.firstName = firstName;
+        this.lastName = lastName;
+    };
+};
+const myPerson2 = new Myperson2('Albi', 'Mudakar');
+console.log(myPerson2);// output : { firstName: "Albi", lastName: "Mudakar" }
+
