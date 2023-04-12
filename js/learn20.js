4941b43623366e2a9d5d0deacef65f0d0d1e71a2
albimdkr <albimudakar04@gmail.com>
2023-04-13 06:46:18 +0700

    add method getter setter  with Regular

--- js/learn20.js
+++ js/learn20.js
@@ -223,3 +223,51 @@ console.log(setterPerson1.getFullName());
 console.log(setterPerson1.score);
 console.log(setterPerson1.skills);
 
+//with regular method called getPeronInfo()
+class RegularMethodPerson {
+    constructor(firstName, lastName, age, country, city){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.country = country;
+        this.city = city;
+        this.score = 0;
+        this.skills = [];
+    };
+    getFullName(){
+        const fullName = this.firstName + ' ' +this.lastName;
+        return fullName;
+    };
+    get getScore(){
+        return this.score;
+    };
+    get getSkills(){
+        return this.skills;
+    };
+    set setScore(score){
+        this.score += score;
+    };
+    set setSkill(skill){
+        this.skills.push(skill);
+    };
+    getPersonInfo(){
+        let fullName = this.getFullName();
+        let skills = 
+            this.skills.length > 0 &&
+            this.skills.slice(0, this.skills.length - 1).join(', ') + `and ${this.skills[this.skills.length - 1]}`;
+        let formattedSkills = skills ? `He knows ${skills}` : '';
+        let info = `${fullName} is ${this.age}. He lives ${this.city}, ${this.country}. ${formattedSkills}`;
+        return info;
+    };
+};
+
+const regularMethodPerson1 = new RegularMethodPerson('Albi', 'Mudakar', 20, 'Indonesian', 'Bandung');
+regularMethodPerson1.setScore = 100;
+regularMethodPerson1.setSkill = 'HTML';
+regularMethodPerson1.setSkill = 'CSS';
+regularMethodPerson1.setSkill = 'JavaScript';
+
+console.log(regularMethodPerson1.getScore);
+console.log(regularMethodPerson1.getSkills);
+console.log(regularMethodPerson1.getPersonInfo());
+
