861631702a0d8f7d932cf5eb52e902cc34e246a8
albimdkr <albimudakar04@gmail.com>
2023-04-14 22:00:23 +0700

    add static method

--- js/learn20.js
+++ js/learn20.js
@@ -250,24 +250,96 @@ class RegularMethodPerson {
     set setSkill(skill){
         this.skills.push(skill);
     };
-    getPersonInfo(){
-        let fullName = this.getFullName();
-        let skills = 
-            this.skills.length > 0 &&
-            this.skills.slice(0, this.skills.length - 1).join(', ') + `and ${this.skills[this.skills.length - 1]}`;
-        let formattedSkills = skills ? `He knows ${skills}` : '';
-        let info = `${fullName} is ${this.age}. He lives ${this.city}, ${this.country}. ${formattedSkills}`;
-        return info;
-    };
+    getPersonInfo() {
+        let fullName = this.getFullName()
+        let skills =
+          this.skills.length > 0 &&
+          this.skills.slice(0, this.skills.length - 1).join(', ') +
+            ` and ${this.skills[this.skills.length - 1]}`
+        let formattedSkills = skills ? `He knows ${skills}` : ''
+    
+        let info = `${fullName} is ${this.age}. He lives ${this.city}, ${this.country}. ${formattedSkills}`
+        return info
+      }
 };
-
 const regularMethodPerson1 = new RegularMethodPerson('Albi', 'Mudakar', 20, 'Indonesian', 'Bandung');
 regularMethodPerson1.setScore = 100;
-regularMethodPerson1.setSkill = 'HTML';
-regularMethodPerson1.setSkill = 'CSS';
-regularMethodPerson1.setSkill = 'JavaScript';
+regularMethodPerson1.setSkill = 'Node.JS';
+regularMethodPerson1.setSkill = 'Express';
+regularMethodPerson1.setSkill = 'MongoDB';
 
 console.log(regularMethodPerson1.getScore);
 console.log(regularMethodPerson1.getSkills);
-console.log(regularMethodPerson1.getPersonInfo());
+console.log(regularMethodPerson1);
+
+//Static Method
+class MyPerson2 {
+    constructor (firstName, lastName, age, country, city){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.country = country;
+        this.city = city;
+        this.score = 0;
+        this.skills = [];
+    };
+    getFullName(){
+        const fullName = this.firstName + ' ' + this.lastName;
+        return fullName;
+    };
+    get getScore(){
+        return this.score;
+    };
+    get getSkills(){
+        return this.skills;
+    };
+    set setScore(score){
+        this.score+=score;
+    };
+    set setSkill(skill){
+        this.skills.push(skill);
+    };
+
+    getPersonInfo() {
+        let fullName = this.getFullName()
+        let skills =
+          this.skills.length > 0 &&
+          this.skills.slice(0, this.skills.length - 1).join(', ') +
+            ` and ${this.skills[this.skills.length - 1]}`
+    
+        let formattedSkills = skills ? `He knows ${skills}` : ''
+    
+        let info = `${fullName} is ${this.age}. He lives ${this.city}, ${this.country}. ${formattedSkills}`
+        return info
+      };
+
+      static favoriteSkill(){
+        const skills = ['HTML', 'CSS', 'JS', 'React', 'Python', 'Node'];
+        const index = Math.floor(Math.random() * skills.length);
+        return skills[index];
+      };
+
+      static showDateTime(){
+        let now = new Date();
+        let year = now.getFullYear();
+        let month = now.getMonth() + 1;
+        let date = now.getDate();
+        let hours = now.getHours();
+        let minutes = now.getMinutes();
+        if (hours < 10){
+            hours = '0' + hours;
+        }
+        if (minutes < 10){
+            minutes = '0' + minutes;
+        }
+
+        let dateMonthYear = date + '.' + month + '.' + year;
+        let time = hours + ':' + minutes;
+        let fullTime = dateMonthYear + ' ' + time;
+        return fullTime;
+      };
+};
+console.log(MyPerson2.favoriteSkill());
+console.log(MyPerson2.showDateTime());
+
 
