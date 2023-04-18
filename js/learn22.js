29e22b1c0d45c693dae469fd9a540555e06a7c54
albimdkr <albimudakar04@gmail.com>
2023-04-18 19:34:42 +0700

    add convert object to JSON

--- js/learn22.js
+++ js/learn22.js
@@ -39,6 +39,78 @@ const userObj1 = JSON.parse(usersText, (key, value) => {
 console.log(userObj1);
 
 
+//Converting Object to JSON
+// JSON.stringify(obj, replacer, space)
+const users = {
+    Albi: {
+        email : 'albi@alb.com',
+        skills : ['HTML', 'CSS', 'JavaScript'],
+        age : 20,
+        isLoggedIn : false,
+        points : 30
+    },
+    Asab: {
+        email: 'asab@asab.com',
+        skills: [
+          'HTML',
+          'CSS',
+          'JavaScript',
+          'Redux',
+          'MongoDB',
+          'Express',
+          'React',
+          'Node'
+        ],
+        age: 25,
+        isLoggedIn: false,
+        points: 50
+      },
+      Brook: {
+        email: 'daniel@daniel.com',
+        skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
+        age: 30,
+        isLoggedIn: true,
+        points: 50
+      },
+      Daniel: {
+        email: 'daniel@alex.com',
+        skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
+        age: 20,
+        isLoggedIn: false,
+        points: 40
+      },
+      John: {
+        email: 'john@john.com',
+        skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
+        age: 20,
+        isLoggedIn: true,
+        points: 50
+      },
+      Thomas: {
+        email: 'thomas@thomas.com',
+        skills: ['HTML', 'CSS', 'JavaScript', 'React'],
+        age: 20,
+        isLoggedIn: false,
+        points: 40
+      },
+      Paul: {
+        email: 'paul@paul.com',
+        skills: [
+          'HTML',
+          'CSS',
+          'JavaScript',
+          'MongoDB',
+          'Express',
+          'React',
+          'Node'
+        ],
+        age: 20,
+        isLoggedIn: false,
+        points: 40
+      }
+}
+const txt = JSON.stringify(users, undefined, 4);
+console.table(txt);
 
 
 
