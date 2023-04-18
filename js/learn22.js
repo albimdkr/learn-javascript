dc337c6e1ed5f41d378bea5bec9f51da296984be
albimdkr <albimudakar04@gmail.com>
2023-04-18 13:56:44 +0700

    add JSON.parse

---
+++ js/learn22.js
@@ -0,0 +1,39 @@
+//JSON
+//Converting JSON to JavaScript Object
+var space = "===============";
+
+//JSON.parse()
+//JSON.parse(json[, reviver])
+
+const usersText = `{
+"users" : [
+    {
+        "firstName" : "Albi",
+        "lastName" : "Mudakar",
+        "age" : 250,
+        "email" : "albi@alb.com"
+    },
+    {
+        "firstName" : "Jack",
+        "lastName" : "Krauser",
+        "age" : 30,
+        "email" : "jack@jck.com"
+    },
+    {
+        "firstName" : "Ada",
+        "lastName" : "Wong",
+        "age" : 22,
+        "email" : "adawg@umbrl.com"
+    }
+]
+}`
+const userObj = JSON.parse (usersText, undefined, 4);
+console.log(userObj);
+
+
+
+
+
+
+
+
