d3c95824971feee5d4e9542e3a4f2fc84255e87c
albimdkr <albimudakar04@gmail.com>
2023-04-19 20:17:29 +0700

    add synchronous

--- asynchronous-javascript/callback.js
+++ asynchronous-javascript/callback.js
@@ -1,1 +1,26 @@
+//CALLBACK
+//Study Case Login Asynchrounus
+const token = ~~[Math.random() * 12345678];
+const pictures = ["1.jpg","2.jpg","3.jpg"];
 
+function login(username, callback){ //supaya bisa dapat value dari sini, simpan paramater callback
+    console.log('Processing validation data...')
+
+    setTimeout(() => { 
+        callback ({token, username});//setup value callback
+    }, 200)
+}
+
+function getUser(token){ 
+    if (token) return {apiKey : "xkey123"};
+}
+
+
+function getPictures (apiKey){
+    if (apiKey) return pictures
+}
+
+
+login('albi_nasyabi', function(){//access parameter callback dengan buat function
+    console.log('getting data!');
+});
--- asynchronous-javascript/default.js
+++ asynchronous-javascript/default.js
@@ -1,3 +1,4 @@
+//CALLBACK
 //Synchronous 
 // function satu(){
 //     console.log("satu");
@@ -20,14 +21,37 @@
 
 //Study Case Login Asynchrounus
 const token = ~~[Math.random() * 12345678];
+const pictures = ["1.jpg","2.jpg","3.jpg"];
 
-function login(username){
-    return {token, username}
+//01. login syarat nya (username)
+function login(username){ //awal, generate token dari login
+    console.log('Processing validation data...')
+
+    //anggaplah ketika login selalu ada jeda waktu delay request ke server anggaplah setTimeout 200ms
+    setTimeout(() => { //1. undifined
+        return {token, username};
+    }, 200)
+    // return {token, username}
 }
 
-function getUser(){
-
+//02. getUser sarannya (token) di ambil dari func login
+function getUser(token){ //kedua, terus dapet apiKey dari getUser
+    if (token) return {apiKey : "xkey123"};
 }
 
-const user = login("albi_nasyabi");
-console.log(user);
+//03. getPicture syaratnya (apiKey) yang didapat dari func getUser ketika berhasil di eksekusi 
+function getPictures (apiKey){//ketiga, terus dapatin picturenya dari getPictures
+    if (apiKey) return pictures
+}
+
+
+const user = login('albi_nasyabi');
+console.log(user); //1. undifined
+
+// const user = login("albi_nasyabi"); //input username = user
+// console.log(user.token);
+// const apiKey = getUser(user.token);
+// console.log(apiKey);
+
+// const getUserPictures = getPictures(apiKey);
+// console.log(getUserPictures);
