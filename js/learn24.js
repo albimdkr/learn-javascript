85f4d565f7e251a63139e2789d9e8b1e7e2a854d
albimdkr <albimudakar04@gmail.com>
2023-04-20 22:05:13 +0700

    add promise constructor if wrong

--- js/learn24.js
+++ js/learn24.js
@@ -32,3 +32,21 @@ const doPromise = new Promise((resolve, reject) => {
 doPromise.then((result) => {
   console.log(result)
 }).catch((err) => console.log(err))
+
+// Promise if wrong
+const doPromise1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    const skills = ['HTML', 'CSS', 'JS']
+    if (skills.includes('Node')) {
+      resolve('fullstack developer')
+    } else {
+      reject('Something wrong has happened')
+    }
+  }, 2000)
+})
+
+doPromise1
+  .then(result => {
+    console.log(result)
+  })
+  .catch(error => console.error(error))
