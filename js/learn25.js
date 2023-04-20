c56bf75311eb4aeded5955920e25941c52060c4a
albimdkr <albimudakar04@gmail.com>
2023-04-21 05:25:58 +0700

    add closure inner func

---
+++ js/learn25.js
@@ -0,0 +1,13 @@
+//CLOSURE
+function outerFunction(){
+    let count = 0;
+    function innerFunction(){
+        count++
+        return count
+    }
+    return innerFunction
+}
+const innerFunc = outerFunction()
+console.log(innerFunc())
+console.log(innerFunc())
+console.log(innerFunc())
