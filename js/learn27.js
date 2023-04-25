1c5d6100153716fb20ef24a3e3f95cdeec60a434
albimdkr <albimudakar04@gmail.com>
2023-04-25 18:09:01 +0700

    innerHTML

--- js/learn27.js
+++ js/learn27.js
@@ -81,5 +81,15 @@ const titles4 = document.querySelectorAll('h1')
 titles4[3].textContent = 'Fourth Title'
 
 //innerHTML
+const lists = `
+        <li>30DaysOfPython Challenge Done</li>
+        <li>30DaysOfJavaScript Challenge Ongoing</li>
+        <li>30DaysOfReact Challenge Coming</li>
+        <li>30DaysOfFullStack Challenge Coming</li>
+        <li>30DaysOfDataAnalysis Challenge Coming</li>
+        <li>30DaysOfReactNative Challenge Coming</li>
+        <li>30DaysOfMachineLearning Challenge Coming</li>`
+const ul = document.querySelector('ul')
+ul.innerHTML = lists
 
 
