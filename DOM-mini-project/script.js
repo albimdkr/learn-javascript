f767086f632a6b254baaad601d0ed6311207e4da
albimdkr <albimudakar04@gmail.com>
2023-05-05 20:09:23 +0700

    add function changeColor

--- DOM-mini-project/script.js
+++ DOM-mini-project/script.js
@@ -1,4 +1,15 @@
-const years = document.getElementsById('year');
-const dateTimeElement = document.getElementById('datetime')
-const challengeElement = document.getElementById('challenge');
+// const years = document.getElementsById('year');
+// const dateTimeElement = document.getElementById('datetime')
+// const challengeElement = document.getElementById('challenge');
+
+function changeColor(){
+    const yearElement = document.getElementById('year');
+    const color = ['red', 'blue', 'green', 'orange'];
+    let index = 0;
+
+    setInterval(() => {
+        yearElement.style.color = colors[index % colors.length];
+        index++;
+    }, 1000);
+}
 
