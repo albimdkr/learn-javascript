f5448d66ff2a8dec276f3128aa5d196c042901c9
albimdkr <albimudakar04@gmail.com>
2023-05-12 17:09:22 +0700

    adding time function

--- DOM-mini-project/script.js
+++ DOM-mini-project/script.js
@@ -1,10 +1,26 @@
+function time(){
+    const now = new Date();
+    const minute = now.getMinutes();
+    const hours = now.getHours();
+    const second = now.getSeconds();
+
+    const year = now.getFullYear();
+    const month = now.getMonth();
+    const date = now.getDate();
+    
+    //const printAll = (`${hours}:${minutes} - ${day}/${month}/${years}/${date}`);
+    const printAll = (`${month}, ${date} ${year} ${hours}:${minute}:${second}`);
+    document.getElementById('date-time').innerHTML = printAll;
+}
+
+
 function changeYearColor(){
     const yearElement = document.getElementById('year');
     const color = ['red', 'blue', 'green', 'orange'];
     let index = 0;
 
     setInterval(() => {
-        yearElement.style.color = colors[index % colors.length];
+        yearElement.style.color = color[index % color.length];
         index++;
     }, 1000);
 }
@@ -20,7 +36,7 @@ function changeDateTimeBackground(){
     }, 1000);
 }
 
-
+time();
 changeYearColor();
 changeDateTimeBackground();
 
