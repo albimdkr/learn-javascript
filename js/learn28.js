fbc6ea2086406512f9cb14b7846f86a3f0d64795
albimdkr <albimudakar04@gmail.com>
2023-05-15 10:09:22 +0700

    Create an Element

---
+++ js/learn28.js
@@ -0,0 +1,7 @@
+//MANIPULATIN DOM
+let title = document.createElement('h3')
+title.className = 'title';
+title.style.fontSize = '24px';
+title.textContent = 'Creating HTML Element DOM Day 2';
+console.log(title);
+
