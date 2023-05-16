ffb7de1e652c76e9f7b235e73b91968dd5a30dde
albimdkr <albimudakar04@gmail.com>
2023-05-16 12:09:22 +0700

    Creating elements

--- js/learn28.js
+++ js/learn28.js
@@ -5,3 +5,12 @@ title.style.fontSize = '24px';
 title.textContent = 'Creating HTML Element DOM Day 2';
 console.log(title);
 
+//Creating elements
+let title1;
+for (let i = 0; i < 3; i++){
+    title1 = document.createElement('h1');
+    title1.className = 'title';
+    title1.style.fontSize = '24px';
+    title1.textContent = i;
+    console.log(title1);
+}
