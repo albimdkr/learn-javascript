e2b5bdccb612fb717b5875201b0cc056526840b6
albimdkr <albimudakar04@gmail.com>
2023-05-17 07:22:39 +0700

    add appendingchild

--- js/learn28.js
+++ js/learn28.js
@@ -9,8 +9,19 @@ console.log(title);
 let title1;
 for (let i = 0; i < 3; i++){
     title1 = document.createElement('h1');
-    title1.className = 'title';
+    title1.className = 'title1';
     title1.style.fontSize = '24px';
     title1.textContent = i;
     console.log(title1);
+}
+
+//Appending child to a parent element
+let title2;
+for (let i = 0; i < 3; i++){
+    title2 = document.createElement('h3');
+    title2.className = 'title2';
+    title2.style.fontSize = '24';
+    title2.style.color = 'white';
+    title2.textContent = i;
+    document.body.appendChild(title2);
 }
