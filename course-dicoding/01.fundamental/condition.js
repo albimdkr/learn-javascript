9a62a1dee8a35a3a46895693398e15ecaea5a42a
albimdkr <albimudakar04@gmail.com>
2023-05-19 20:09:12 +0700

    update

--- course-dicoding/01.fundamental/condition.js
+++ course-dicoding/01.fundamental/condition.js
@@ -11,3 +11,63 @@ Persiapan sebelum berangkat kegiatan.
 Hari ini hujan. Bawa payung.
 Berangkat kegiatan.
  */
+
+let x = 50;
+
+if(x > 70) {
+    console.log(x);
+} else {
+    console.log("Nilai kurang dari 70");
+}
+
+/* output
+Nilai kurang dari 70
+*/
+
+let language = "French";
+let greeting = "Selamat Pagi"
+
+if(language === "English") {
+    greeting = "Good Morning!";
+} else if(language === "French") {
+    greeting = "Bonjour!"
+} else if(language === "Japanese") {
+    greeting = "Ohayou Gozaimasu!"
+}
+
+console.log(greeting);
+
+/* output
+Bonjour!
+*/
+
+// condition ? true expression : false expression
+
+const isMember = false;
+const discount = isMember ? 0.1 : 0;
+console.log(`Anda mendapatkan diskon sebesar ${discount * 100}%`)
+
+/* output
+Anda mendapatkan diskon sebesar 0%
+ */
+
+// Di dalam if statement kita perlu memasukkan expression yang akan dievaluasi. Umumnya, expression tersebut mengembalikan nilai boolean untuk menentukan kondisi true atau false. Lalu bagaimana jika kita menuliskan expression yang tidak mengembalikan nilai boolean? Jawabannya bisa.
+// Setiap nilai pada JavaScript pada dasarnya juga mewarisi sifat boolean. Nilai ini dikenal dengan truthy atau falsy. Nilai truthy berarti nilai yang ketika dievaluasi akan menghasilkan nilai true, begitu pula falsy bernilai false. Jadi manakah yang termasuk truthy dan falsy? Selain nilai boolean false, tipe data atau nilai yang dianggap falsy, antara lain:
+//     Number 0
+//     BigInt 0n
+//     String kosong seperti “” atau ‘’
+//     null
+//     undefined
+//     NaN, atau Not a Number
+
+let name = "";
+
+if (name) {
+    console.log(`Halo, ${name}`);
+} else {
+    console.log("Nama masih kosong");
+}
+
+/* output:
+Nama masih kosong
+ */
