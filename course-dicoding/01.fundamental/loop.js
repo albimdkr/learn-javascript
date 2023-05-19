6aecd37eba4f4c53d2cacb6c90b775c8cc897561
albimdkr <albimudakar04@gmail.com>
2023-05-19 20:09:12 +0700

    complete fundamental

--- course-dicoding/01.fundamental/loop.js
+++ course-dicoding/01.fundamental/loop.js
@@ -11,4 +11,31 @@ for(let i = 0; i < 5; i++) {
   4
   */
 
-  
+  // FOR OF LOOP
+let myArray = ["Luke", "Han", "Chewbacca", "Leia"];
+
+for(const arrayItem of myArray) {
+  console.log(arrayItem)
+}
+
+/* output
+Luke
+Han
+Chewbacca
+Leia
+*/
+
+// WHILE AND DO WHILE
+let i = 1;
+
+while (i <= 100) {
+  console.log(i);
+  i++;
+}
+
+// INFINITE LOOP
+let a = 1;
+     
+while (a <= 5) {
+  console.log(i);
+}
