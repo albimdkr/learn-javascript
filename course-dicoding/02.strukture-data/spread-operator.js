f0ad54fd8d72506dfb56d6e9a13a0966973fc90b
albimdkr <albimudakar04@gmail.com>
2023-05-20 09:01:02 +0700

    add weakmap-weakset

--- course-dicoding/02.strukture-data/spread-operator.js
+++ course-dicoding/02.strukture-data/spread-operator.js
@@ -32,3 +32,15 @@ console.log(allFavoritesAfter);
 /* output
 [ 'Seafood', 'Salad', 'Nugget', 'Soup', 'Cake', 'Pie', 'Donut' ]
 */
+
+// OBJECT LITERALS
+const obj1 = { firstName: 'Obi-Wan', age: 30 };
+const obj2 = { lastName: 'Kenobi', gender: 'M' };
+
+const newObj = { ...obj1, ...obj2 };
+
+console.log(newObj);
+
+/* output
+{ firstName: 'Obi-Wan', age: 30, lastName: 'Kenobi', gender: 'M' }
+*/
