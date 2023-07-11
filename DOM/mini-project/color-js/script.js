a28a8941e2f7f12129b5e9fe173cf79c8c4f2d47
albimdkr <albimudakar04@gmail.com>
2023-07-11 13:45:21 +0700

    adding classList toggle

---
+++ DOM/mini-project/color-js/script.js
@@ -0,0 +1,10 @@
+// const btn = document.querySelector('section button');
+// btn.addEventListener('onclick', function(){
+    //     body.style.backgroundColor = 'lightblue';
+    // });
+    
+    
+const btn = document.querySelector('section button');
+btn.onclick = function (){
+    document.body.classList.toggle('biru-muda');
+};
