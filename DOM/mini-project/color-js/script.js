17ad8327d0b92ea8e4f601e832de76f14f338002
albimdkr <albimudakar04@gmail.com>
2023-07-12 13:45:21 +0700

    adding r, for random color

--- DOM/mini-project/color-js/script.js
+++ DOM/mini-project/color-js/script.js
@@ -1,10 +1,23 @@
-// const btn = document.querySelector('section button');
-// btn.addEventListener('onclick', function(){
-    //     body.style.backgroundColor = 'lightblue';
-    // });
-    
-    
+//case 1
 const btn = document.querySelector('section button');
 btn.onclick = function (){
     document.body.classList.toggle('biru-muda');
 };
+
+
+//case 2
+const btnRandom = document.createElement('button');
+const btnValue = document.createTextNode('Random Color');
+
+btnRandom.appendChild(btnValue);
+btnRandom.setAttribute('type', 'button');
+btn.after(btnRandom);
+
+btnRandom.addEventListener('click', function(){
+    const r = Math.round(Math.random() * 255 + 1);
+    document.body.style.backgroundColor = 'rgb('+ r +',100,100)';
+    //alert(r);
+});
+
+
+
