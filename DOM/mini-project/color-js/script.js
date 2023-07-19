21083751b8da038448ae96ea6ef5df76af9cf0d1
albimdkr <albimudakar04@gmail.com>
2023-07-19 21:00:11 +0700

    adding input:name = b

--- DOM/mini-project/color-js/script.js
+++ DOM/mini-project/color-js/script.js
@@ -1,11 +1,11 @@
-//case 1
+//case 1 > btnClick change backgroundColor
 const btn = document.querySelector('section button');
 btn.onclick = function (){
     document.body.classList.toggle('biru-muda');
 };
 
 
-//case 2
+//case 2 > btn Random backgroundColor
 const btnRandom = document.createElement('button');
 const btnValue = document.createTextNode('Random Color');
 
@@ -18,18 +18,30 @@ btnRandom.addEventListener('click', function(){
     document.body.style.backgroundColor = 'rgb('+ r +',100,100)';
 });
 
+
+//case 3 > with input type change backgroundColor
 const sMerah = document.querySelector('input[name=sMerah]');
 const sHijau = document.querySelector('input[name=sHijau]');
+const sBiru = document.querySelector('input[name=sBiru]');
 sMerah.addEventListener('input', function() {
     const r = sMerah.value;
     const g = sHijau.value;
-    document.body.style.backgroundColor = 'rgb('+r+','+g+',100)';
+    const b = sBiru.value;
+    document.body.style.backgroundColor = 'rgb('+r+','+g+','+b+')';
 });
 
 sHijau.addEventListener('input', function() {
     const r = sMerah.value;
     const g = sHijau.value;
-    document.body.style.backgroundColor = 'rgb('+r+','+g+',100)';
+    const b = sBiru.value;
+    document.body.style.backgroundColor = 'rgb('+r+','+g+','+b+')';
+ });
+
+ sBiru.addEventListener('input', function() {
+    const r = sMerah.value;
+    const g = sHijau.value;
+    const b = sBiru.value;
+    document.body.style.backgroundColor = 'rgb('+r+','+g+','+b+')';
  });
 
 
