d72dc514c185e97763018811a6de170d98037cee
albimdkr <albimudakar04@gmail.com>
2023-07-26 02:59:05 +0700

    adding Traversal using method parentElement and using forEach for efficiency code

--- DOM/learn/traversal/script.js
+++ DOM/learn/traversal/script.js
@@ -1,11 +1,30 @@
-//Traversal
-const container = document.getElementsByClassName('container')[0];
-const closeIcon1 = document.getElementsByTagName('span')[2];
+//TRAVERSAL
+//Penulusuran, Searcing, galeungan sawah:v by DOM tree
+
+//DOM STANDAR, by default choose 1
+// const container = document.getElementsByClassName('container')[0];
+// const card = document.getElementsByClassName('card')[0];
+// const closeIcon1 = document.getElementsByTagName('span')[2];
+
+// closeIcon1.addEventListener('click', function(){
+//     container.removeChild(card);
+// });
 
 
-closeIcon1.addEventListener('click', function(){
-    const card = document.getElementsByClassName('card')[0];
-    container.removeChild(card);
+//DOM TRAVERSAL
+// Traversal : parentElement
+// const closeIcon = document.querySelectorAll('.close');
+// for (let i = 0; closeIcon.length; i++){
+//     closeIcon[i].addEventListener('click', function(e){
+//         // closeIcon[i].parentElement.style.display = 'none'; opsi 1
+//         // e.target.parentElement.style.display = 'none'; opsi 2
+//     });
+// };
+
+//Efficiency Code using ForEach
+const closeIcon = document.querySelectorAll('.close');
+closeIcon.forEach(function(element){
+    element.addEventListener('click', function(e){
+        e.target.parentElement.style.display = 'none';
+    });
 });
-
-
