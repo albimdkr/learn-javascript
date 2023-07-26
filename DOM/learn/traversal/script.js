1df4cdaa7ee2ddb8f73add5a477dac3a02f561db
albimdkr <albimudakar04@gmail.com>
2023-07-27 01:37:23 +0700

    adding method traversal

--- DOM/learn/traversal/script.js
+++ DOM/learn/traversal/script.js
@@ -12,8 +12,6 @@
 
 
 //DOM TRAVERSAL
-// parentNode > node  |  parentElement > element  |  nextSibling > node 
-// | nextElementSibling > element  |  previousSibling > node  |   previousElementSibling > element
 // const closeIcon = document.querySelectorAll('.close');
 // for (let i = 0; closeIcon.length; i++){
 //     closeIcon[i].addEventListener('click', function(e){
@@ -21,6 +19,31 @@
 //         // e.target.parentElement.style.display = 'none'; opsi 2
 //     });
 // };
+
+// DOM TRAVERSAL METHOD
+// parentNode > node
+
+// parentElement > element 
+const namaParentElement = document.querySelector('.nama');
+console.log(namaParentElement.parentElement); //mengambil bapak
+// const namaParentElement = document.querySelector('.nama');
+// console.log(namaParentElement.parentElement.parentElement); //mengambil kakek
+
+// nextSibling > node 
+const catchSiblingNameNode = document.querySelector('.nama');
+console.log(catchSiblingNameNode.nextSibling); //mengambil saudara kandung (kebawah), maka akan membaca node nya
+
+// nextElementSibling > element
+const catchSiblingNameElement = document.querySelector('.nama');
+console.log(catchSiblingNameElement.nextElementSibling); //mengambil saudara kandung (kebawah), degan lansung menagmabil element nya
+
+// previousSibling > node 
+const catchPreviousSibling = document.querySelector('.nama');
+console.log(catchPreviousSibling.previousSibling); //mengambil saudara kandung menggunakan node (sebelumnya), maka akan dibacanya node
+
+// previousElementSibling > element
+const catchPreviousElementSibling = document.querySelector('.nama');
+console.log(catchPreviousElementSibling.previousElementSibling); //mengambil saudara kandung menggunakan node (sebelumnya), maka akan dibacanya node
 
 //Efficiency Code using ForEach
 const closeIcon = document.querySelectorAll('.close');
